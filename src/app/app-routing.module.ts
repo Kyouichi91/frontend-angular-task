a455dcea41f17bb5b5761c8bdc39dc3cf3feb2d3
unknown <m4ximskr@gmail.com>
2019-04-05 00:33:16 +0300

    trying to upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompanyListComponent } from './pages/list/company-list.component';
+import { CompanyDetailComponent } from './pages/company/company-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: CompanyListComponent },
+  { path: 'company/:company_name', component: CompanyDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyCardComponent } from './components/company-card/company-card.component';
+import { AddCompanyButtonComponent } from './components/add-company-button/add-company-button.component';
+import { CompaniesSearchComponent } from './components/companies-search/companies-search.component';
+
+import { CompaniesService } from './shared/services/companies.service'
+import { CompanySearchService } from './shared/services/company-search.service'
+import { StatusPipe } from './shared/pipes/status.pipe';
+import { SearchPipe } from './shared/pipes/search.pipe';
+import { ViewDirective } from './shared/directives/view.directive';
+import { CompanyListComponent } from './pages/list/company-list.component';
+import { CompanyDetailComponent } from './pages/company/company-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyCardComponent,
+    AddCompanyButtonComponent,
+    CompaniesSearchComponent,
+    StatusPipe,
+    SearchPipe,
+    ViewDirective,
+    CompanyListComponent,
+    CompanyDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CompaniesService,CompanySearchService],
+  bootstrap: [AppComponent],
+  exports: [CompanyCardComponent,AddCompanyButtonComponent, CompaniesSearchComponent, CompanyListComponent, CompanyDetailComponent]
 })
 export class AppModule { }
